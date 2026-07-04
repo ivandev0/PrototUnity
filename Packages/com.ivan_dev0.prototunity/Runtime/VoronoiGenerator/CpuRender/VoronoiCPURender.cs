3bfc71e41395669318a0a8842e803d423b7c6760
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2026-07-04 10:00:28 +0200

    Move Voronoi CPU render into separate script

---
+++ Packages/com.ivan_dev0.prototunity/Runtime/VoronoiGenerator/CpuRender/VoronoiCPURender.cs
@@ -0,0 +1,94 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace PrototUnity.VoronoiGenerator.CpuRender {
+	public class VoronoiCPURender : MonoBehaviour {
+		[SerializeField] private VoronoiComputeGenerator voronoiGenerator;
+
+		private const string generatedCellPrefix = "VoronoiCell_";
+
+		private void Start() {
+			Generate();
+		}
+
+		[ContextMenu("Generate Voronoi Mesh")]
+		public void Generate() {
+			ClearGeneratedCells();
+			voronoiGenerator.Generate();
+			var (cells, vertices) = voronoiGenerator.GetGeneratedData();
+			BuildMeshesFromComputeData(cells, vertices);
+		}
+		
+		private void BuildMeshesFromComputeData(VoronoiComputeGenerator.VoronoiCell[] cells, Vector3[] vertices) {
+			ClearGeneratedCells();
+			var generatedMaterial = GenerateMaterial();
+			
+			for (var cellIndex = 0; cellIndex < cells.Length; cellIndex++) {
+				var cell = cells[cellIndex];
+
+				var mesh = BuildCellMesh(cellIndex, cell, vertices);
+				if (mesh == null) continue;
+
+				var cellObject = new GameObject($"{generatedCellPrefix}{cellIndex}");
+				cellObject.transform.SetParent(transform, worldPositionStays: false);
+				cellObject.transform.localRotation = Quaternion.identity;
+				cellObject.transform.localScale = Vector3.one;
+
+				var cellMeshFilter = cellObject.AddComponent<MeshFilter>();
+				cellMeshFilter.sharedMesh = mesh;
+
+				var cellMeshRenderer = cellObject.AddComponent<MeshRenderer>();
+				cellMeshRenderer.sharedMaterial = generatedMaterial;
+			}
+		}
+
+		private static Mesh BuildCellMesh(int index, VoronoiComputeGenerator.VoronoiCell cell, IReadOnlyList<Vector3> sourceVertices) {
+			if (cell.vertexCount < 3) return null;
+
+			var vertices = new List<Vector3>((int) cell.vertexCount);
+			var triangles = new List<int>((int) cell.vertexCount);
+
+			for (var i = 0; i < cell.vertexCount; i++) {
+				vertices.Add(sourceVertices[i + (int) cell.vertexStart]);
+			}
+
+			for (var i = 0; i < cell.vertexCount; i++) {
+				triangles.Add(i);
+			}
+
+			var mesh = new Mesh {
+				name = $"Voronoi Cell {index}",
+				indexFormat = IndexFormat.UInt32
+			};
+			mesh.SetVertices(vertices);
+			mesh.SetTriangles(triangles, 0);
+			mesh.RecalculateBounds();
+			mesh.RecalculateNormals();
+			return mesh;
+		}
+
+		private void ClearGeneratedCells() {
+			for (var i = transform.childCount - 1; i >= 0; i--) {
+				var child = transform.GetChild(i);
+				if (!child.name.StartsWith(generatedCellPrefix)) continue;
+
+				DestroyGameObject(child.gameObject);
+			}
+		}
+
+		private Material GenerateMaterial() {
+			var shader = Shader.Find("Universal Render Pipeline/Lit");
+			if (shader == null) shader = Shader.Find("Standard");
+			return new Material(shader) {
+				name = "Generated Voronoi Mesh Material",
+				color = new Color(0.75f, 0.82f, 1f, 1f)
+			};
+		}
+		
+		private static void DestroyGameObject(GameObject target) {
+			if (Application.isPlaying) Destroy(target);
+			else DestroyImmediate(target);
+		}
+	}
+}
--- Packages/com.ivan_dev0.prototunity/Runtime/VoronoiGenerator/VoronoiComputeGenerator.cs
+++ Packages/com.ivan_dev0.prototunity/Runtime/VoronoiGenerator/VoronoiComputeGenerator.cs
@@ -1,20 +1,23 @@
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using PrototUnity.AiTools.Voronoi;
 using SebLague.MarchingCubes;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
-using UnityEngine.Rendering;
 
 namespace PrototUnity.VoronoiGenerator {
 	public class VoronoiComputeGenerator : MonoBehaviour {
-		private const string GeneratedCellPrefix = "VoronoiCell_";
-
 		[SerializeField] private ComputeShader voronoiShader;
 		[SerializeField] private Vector3Int voronoiSize = Vector3Int.one;
 		[SerializeField] private Vector3 boundSize = Vector3.one;
 
-		private struct VoronoiCell {
+		private static readonly int centerGridSizeID = Shader.PropertyToID("_CenterGridSize");
+		private static readonly int worldOriginID = Shader.PropertyToID("_WorldOrigin");
+		private static readonly int boxSizeID = Shader.PropertyToID("_BoxSize");
+		private static readonly int centersID = Shader.PropertyToID("centers");
+		private static readonly int cellsID = Shader.PropertyToID("cells");
+		private static readonly int verticesID = Shader.PropertyToID("vertices");
+		
+		public struct VoronoiCell {
 			public uint vertexCount;
 			public uint vertexStart;
 		}
@@ -27,7 +30,6 @@ namespace PrototUnity.VoronoiGenerator {
 			Generate();
 		}
 
-		[ContextMenu("Generate Voronoi Mesh")]
 		public void Generate() {
 			if (voronoiShader == null) {
 				Debug.LogError("Voronoi compute shader is not assigned.", this);
@@ -40,7 +42,14 @@ namespace PrototUnity.VoronoiGenerator {
 			}
 
 			DispatchVoronoi(voronoiSize);
-			BuildMeshesFromComputeData();
+		}
+
+		public (VoronoiCell[], Vector3[]) GetGeneratedData() {
+			var cells = new VoronoiCell[voronoiCellsBuffer.count];
+			var vertices = new Vector3[voronoiVerticesBuffer.count];
+			voronoiCellsBuffer.GetData(cells);
+			voronoiVerticesBuffer.GetData(vertices);
+			return (cells, vertices);
 		}
 
 		private void DispatchVoronoi(Vector3Int size) {
@@ -53,12 +62,12 @@ namespace PrototUnity.VoronoiGenerator {
 			voronoiCellsBuffer = new ComputeBuffer(numCells, Marshal.SizeOf<VoronoiCell>(), ComputeBufferType.Structured);
 			voronoiVerticesBuffer = new ComputeBuffer(numCells * 255, Marshal.SizeOf<Vector3>(), ComputeBufferType.Structured);
 
-			voronoiShader.SetInts("_CenterGridSize", size.x, size.y, size.z);
-			voronoiShader.SetFloats("_WorldOrigin", 0, 0, 0);
-			voronoiShader.SetFloats("_BoxSize", boundSize.x, boundSize.y, boundSize.z);
-			voronoiShader.SetTexture(kernel, "centers", seedTexture);
-			voronoiShader.SetBuffer(kernel, "cells", voronoiCellsBuffer);
-			voronoiShader.SetBuffer(kernel, "vertices", voronoiVerticesBuffer);
+			voronoiShader.SetInts(centerGridSizeID, size.x, size.y, size.z);
+			voronoiShader.SetFloats(worldOriginID, 0, 0, 0);
+			voronoiShader.SetFloats(boxSizeID, boundSize.x, boundSize.y, boundSize.z);
+			voronoiShader.SetTexture(kernel, centersID, seedTexture);
+			voronoiShader.SetBuffer(kernel, cellsID, voronoiCellsBuffer);
+			voronoiShader.SetBuffer(kernel, verticesID, voronoiVerticesBuffer);
 
 			ComputeHelper.Dispatch(voronoiShader, size.x, size.y, size.z);
 		}
@@ -94,82 +103,6 @@ namespace PrototUnity.VoronoiGenerator {
 			return texture;
 		}
 
-		private void BuildMeshesFromComputeData() {
-			if (voronoiCellsBuffer == null || voronoiVerticesBuffer == null) {
-				Debug.LogError("Voronoi buffers were not created.", this);
-				return;
-			}
-
-			var cells = new VoronoiCell[voronoiCellsBuffer.count];
-			var vertices = new Vector3[voronoiVerticesBuffer.count];
-			voronoiCellsBuffer.GetData(cells);
-			voronoiVerticesBuffer.GetData(vertices);
-
-			ClearGeneratedCells();
-			var generatedMaterial = GenerateMaterial();
-			
-			for (var cellIndex = 0; cellIndex < cells.Length; cellIndex++) {
-				var cell = cells[cellIndex];
-
-				var mesh = BuildCellMesh(cellIndex, cell, vertices);
-				if (mesh == null) continue;
-
-				var cellObject = new GameObject($"{GeneratedCellPrefix}{cellIndex}");
-				cellObject.transform.SetParent(transform, worldPositionStays: false);
-				cellObject.transform.localRotation = Quaternion.identity;
-				cellObject.transform.localScale = Vector3.one;
-
-				var cellMeshFilter = cellObject.AddComponent<MeshFilter>();
-				cellMeshFilter.sharedMesh = mesh;
-
-				var cellMeshRenderer = cellObject.AddComponent<MeshRenderer>();
-				cellMeshRenderer.sharedMaterial = generatedMaterial;
-			}
-		}
-
-		private static Mesh BuildCellMesh(int index, VoronoiCell cell, IReadOnlyList<Vector3> sourceVertices) {
-			if (cell.vertexCount < 3) return null;
-
-			var vertices = new List<Vector3>((int) cell.vertexCount);
-			var triangles = new List<int>((int) cell.vertexCount);
-
-			for (var i = 0; i < cell.vertexCount; i++) {
-				vertices.Add(sourceVertices[i + (int) cell.vertexStart]);
-			}
-
-			for (var i = 0; i < cell.vertexCount; i++) {
-				triangles.Add(i);
-			}
-
-			var mesh = new Mesh {
-				name = $"Voronoi Cell {index}",
-				indexFormat = IndexFormat.UInt32
-			};
-			mesh.SetVertices(vertices);
-			mesh.SetTriangles(triangles, 0);
-			mesh.RecalculateBounds();
-			mesh.RecalculateNormals();
-			return mesh;
-		}
-
-		private void ClearGeneratedCells() {
-			for (var i = transform.childCount - 1; i >= 0; i--) {
-				var child = transform.GetChild(i);
-				if (!child.name.StartsWith(GeneratedCellPrefix)) continue;
-
-				DestroyGameObject(child.gameObject);
-			}
-		}
-
-		private Material GenerateMaterial() {
-			var shader = Shader.Find("Universal Render Pipeline/Lit");
-			if (shader == null) shader = Shader.Find("Standard");
-			return new Material(shader) {
-				name = "Generated Voronoi Mesh Material",
-				color = new Color(0.75f, 0.82f, 1f, 1f)
-			};
-		}
-
 		private void ReleaseBuffers() {
 			if (voronoiCellsBuffer != null) {
 				voronoiCellsBuffer.Release();
@@ -189,7 +122,6 @@ namespace PrototUnity.VoronoiGenerator {
 
 		private void OnDestroy() {
 			ReleaseBuffers();
-			ClearGeneratedCells();
 		}
 
 		private void OnDisable() {
@@ -197,11 +129,6 @@ namespace PrototUnity.VoronoiGenerator {
 		}
 
 		private static void DestroyTexture(Texture target) {
-			if (Application.isPlaying) Destroy(target);
-			else DestroyImmediate(target);
-		}
-
-		private static void DestroyGameObject(GameObject target) {
 			if (Application.isPlaying) Destroy(target);
 			else DestroyImmediate(target);
 		}
