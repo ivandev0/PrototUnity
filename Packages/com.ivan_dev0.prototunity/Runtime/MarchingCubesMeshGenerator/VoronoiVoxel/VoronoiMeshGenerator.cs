f7d329aad34c52dcc030f1a1f014672ea427ad74
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2026-07-05 21:59:11 +0200

    Use `VoronoiComputeGenerator` instead of copy pasted `Dispatch` method

--- Packages/com.ivan_dev0.prototunity/Runtime/MarchingCubesMeshGenerator/VoronoiVoxel/VoronoiMeshGenerator.cs
+++ Packages/com.ivan_dev0.prototunity/Runtime/MarchingCubesMeshGenerator/VoronoiVoxel/VoronoiMeshGenerator.cs
@@ -7,18 +7,13 @@ using UnityEngine.Rendering;
 namespace PrototUnity.MarchingCubesMeshGenerator.VoronoiVoxel {
 	[RequireComponent(typeof(MeshCollider))]
 	public class VoronoiMeshGenerator : AbstractMeshGenerator {
+		[SerializeField] private AbstractVoronoiComputeGenerator voronoiGenerator;
 		[SerializeField] private ComputeShader voxelShader;
 		
 		[SerializeField] private ComputeShader voronoiShader;
 		[SerializeField] private Material voronoiMaterial;
 		
 		private int NumVoxels => (NumPointsPerAxis - 1) * (NumPointsPerAxis - 1) * (NumPointsPerAxis - 1);
-		
-		struct VoronoiCell
-		{
-			public uint vertexCount;
-			public uint vertexStart;
-		};
 		
 		private struct Voxel {
 			private uint id;
@@ -28,8 +23,6 @@ namespace PrototUnity.MarchingCubesMeshGenerator.VoronoiVoxel {
 		private ComputeBuffer voxelCountBuffer;
 		private ComputeBuffer voxelsBuffer;
 		private Texture3D colorTexture;
-		private ComputeBuffer voronoiCellsBuffer;
-		private ComputeBuffer voronoiVerticesBuffer;
 
 		private MeshCollider meshCollider;
 		private Mesh voronoiMesh;
@@ -38,16 +31,13 @@ namespace PrototUnity.MarchingCubesMeshGenerator.VoronoiVoxel {
 		private static readonly int numPointsPerAxisID = Shader.PropertyToID("numPointsPerAxis");
 		private static readonly int colorsID = Shader.PropertyToID("colors");
 		private static readonly int colorSizeID = Shader.PropertyToID("colorSize");
-		private static readonly int centerGridSizeID = Shader.PropertyToID("_CenterGridSize");
-		private static readonly int worldOriginID = Shader.PropertyToID("_WorldOrigin");
-		private static readonly int boxSizeID = Shader.PropertyToID("_BoxSize");
-		private static readonly int centersID = Shader.PropertyToID("centers");
 		private static readonly int cellsID = Shader.PropertyToID("cells");
 		private static readonly int verticesID = Shader.PropertyToID("vertices");
 		private static readonly int idsToRenderID = Shader.PropertyToID("idsToRender");
 
 		public override void BeforePointGeneration() {
 			meshCollider = GetComponent<MeshCollider>();
+			voronoiGenerator.Generate();
 			CreateBuffers();
 		}
 
@@ -55,16 +45,13 @@ namespace PrototUnity.MarchingCubesMeshGenerator.VoronoiVoxel {
 			voxelCountBuffer = new ComputeBuffer(1, sizeof(int), ComputeBufferType.Raw);
 			voxelsBuffer = new ComputeBuffer(NumVoxels, Marshal.SizeOf(typeof(Voxel)), ComputeBufferType.Append);
 			
-			voronoiCellsBuffer = new ComputeBuffer(NumVoxels, Marshal.SizeOf<VoronoiCell>(), ComputeBufferType.Structured);
-			voronoiVerticesBuffer = new ComputeBuffer(NumVoxels * 255, Marshal.SizeOf<Vector3>(), ComputeBufferType.Structured);
-			
 			voronoiMaterial.enableInstancing = true;
 			voronoiMaterial.SetBuffer(idsToRenderID, voxelsBuffer);
 			colorTexture = VoronoiGenerator.Utils.CreateRandomColors(0, NumPointsPerAxis, NumPointsPerAxis, NumPointsPerAxis);
 			voronoiMaterial.SetTexture(colorsID, colorTexture);
 			voronoiMaterial.SetVector(colorSizeID, new Vector4(NumPointsPerAxis, NumPointsPerAxis, NumPointsPerAxis, 0));
-			voronoiMaterial.SetBuffer(cellsID, voronoiCellsBuffer);
-			voronoiMaterial.SetBuffer(verticesID, voronoiVerticesBuffer);
+			voronoiMaterial.SetBuffer(cellsID, voronoiGenerator.VoronoiCellsBuffer);
+			voronoiMaterial.SetBuffer(verticesID, voronoiGenerator.VoronoiVerticesBuffer);
 			
 			CreateVoxelEmptyMesh();
 		}
@@ -84,28 +71,12 @@ namespace PrototUnity.MarchingCubesMeshGenerator.VoronoiVoxel {
 
 		public override void AfterPointGeneration() {
 			InitTextures();
-			DispatchVoronoi(Vector3Int.one * (NumPointsPerAxis - 1));
 		}
 
 		private void InitTextures() {
 			// TODO this is slightly incorrect. We want to filter voxels that are "between" points, not "on" them.
 			//  This results in visually incorrect mesh generation.
 			voxelShader.SetTexture(0, pointsID, pointsBuffer);
-		}
-		
-		// TODO use voronoi generator instead
-		private void DispatchVoronoi(Vector3Int size) {
-			var seedTexture = VoronoiComputeGenerator.GenerateSeedTexture(size, boundSize);
-			var kernel = voronoiShader.FindKernel("BuildVoronoiCells3D");
-
-			voronoiShader.SetInts(nameID: centerGridSizeID, size.x, size.y, size.z);
-			voronoiShader.SetFloats(worldOriginID, 0, 0, 0);
-			voronoiShader.SetFloats(boxSizeID, boundSize.x, boundSize.y, boundSize.z);
-			voronoiShader.SetTexture(kernel, centersID, seedTexture);
-			voronoiShader.SetBuffer(kernel, cellsID, voronoiCellsBuffer);
-			voronoiShader.SetBuffer(kernel, verticesID, voronoiVerticesBuffer);
-
-			ComputeHelper.Dispatch(voronoiShader, size.x, size.y, size.z);
 		}
 
 		public override void AfterMeshGeneration() {
@@ -156,8 +127,6 @@ namespace PrototUnity.MarchingCubesMeshGenerator.VoronoiVoxel {
 			base.ReleaseBuffers();
 			voxelCountBuffer?.Release();
 			voxelsBuffer?.Release();
-			voronoiCellsBuffer?.Release();
-			voronoiVerticesBuffer?.Release();
 		}
 	}
 }
