70a31831044e70d19d10146e77affef24a1d4ecc
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2026-07-15 23:50:56 +0200

    Assign random colors to each voxel using shader graph

--- Packages/com.ivan_dev0.prototunity/Runtime/MarchingCubesMeshGenerator/MeshVoxel/Shader/CustomNode.hlsl
+++ Packages/com.ivan_dev0.prototunity/Runtime/MarchingCubesMeshGenerator/MeshVoxel/Shader/CustomNode.hlsl
@@ -16,4 +16,20 @@ void GetVoxelPosition_float(uint instanceId, out float3 voxelPosition)
     voxelPosition = voxels[instanceId].position;
 }
 
+int3 IdToCoord(uint instanceID)
+{
+    uint idOfCell = voxels[instanceID].id;
+    uint width = colorSize.x, height = colorSize.y;
+    int x = idOfCell % width;
+    int y = (idOfCell / width) % height;
+    int z = idOfCell / (width * height);
+    return int3(x, y, z); 
+}
+
+void GetColor_float(uint instanceID, out float4 color)
+{
+    int3 coord = IdToCoord(instanceID);
+    color = colors.Load(int4(coord, 0));
+}
+
 #endif // VOXELS
