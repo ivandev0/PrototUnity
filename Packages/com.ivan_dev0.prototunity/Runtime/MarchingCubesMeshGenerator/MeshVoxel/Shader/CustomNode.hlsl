7a6f548f3a0fd21287727eed61c22fad6f826724
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2026-07-16 00:01:41 +0200

    Apply different height to voxels using shader graph

--- Packages/com.ivan_dev0.prototunity/Runtime/MarchingCubesMeshGenerator/MeshVoxel/Shader/CustomNode.hlsl
+++ Packages/com.ivan_dev0.prototunity/Runtime/MarchingCubesMeshGenerator/MeshVoxel/Shader/CustomNode.hlsl
@@ -10,11 +10,7 @@ struct Voxel
 StructuredBuffer<Voxel> voxels;
 Texture3D colors;
 float4 colorSize;
-
-void GetVoxelPosition_float(uint instanceId, out float3 voxelPosition)
-{
-    voxelPosition = voxels[instanceId].position;
-}
+Texture2D<float> heights;
 
 int3 IdToCoord(uint instanceID)
 {
@@ -24,6 +20,13 @@ int3 IdToCoord(uint instanceID)
     int y = (idOfCell / width) % height;
     int z = idOfCell / (width * height);
     return int3(x, y, z); 
+}
+
+void GetVoxelPosition_float(uint instanceId, out float3 voxelPosition)
+{
+    int3 coord = IdToCoord(instanceId);
+    float height = heights.Load(int3(coord[0], coord[2], 0));
+    voxelPosition = voxels[instanceId].position + float3(0, height, 0);
 }
 
 void GetColor_float(uint instanceID, out float4 color)
